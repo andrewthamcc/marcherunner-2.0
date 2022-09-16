06b0693b4b19d463d260a8cd73b10f1bfd84e6ae
andrewthamcc <andrew.tham.cc@gmail.com>
2022-09-16 14:19:00 -0400

    fix import

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import PrivateRoute from '../auth/private-route/private-route'
+import PrivateRoute from '../auth'
 import { Dashboard } from './dashboard'
 import { Home } from './home'
 
