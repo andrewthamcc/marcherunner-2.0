bec51a90eb5ed8da5f5bcacb352abebd86bf2fa8
Andrew Tham <45271739+andrewthamcc@users.noreply.github.com>
2022-09-20 22:30:46 -0400

    404 Page (#24)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import { PrivateRoute } from '../auth'
 import { Dashboard } from './dashboard'
 import { Home } from './home'
+import { NotFound } from './404'
 
 const Pages = () => {
   return (
     <Switch>
       <Route component={Home} exact path="/" />
       <PrivateRoute component={Dashboard} path="/dashboard" />
+      <Route component={NotFound} />
     </Switch>
   )
 }
