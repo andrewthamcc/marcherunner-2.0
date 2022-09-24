13e498739227ee460f9adf34eb8f89af1e2ae6a8
andrewthamcc <andrew.tham.cc@gmail.com>
2022-09-24 17:38:27 -0400

    docker database for local development

--- backend/src/prismaClient.ts
+++ backend/src/prismaClient.ts
@@ -1,6 +1,21 @@
+import path from 'path'
+import dotenv from 'dotenv'
 import { PrismaClient } from '@prisma/client'
+dotenv.config({ path: path.resolve('./.env') })
 
-const prisma = new PrismaClient()
+const env = process.env.NODE_ENV || 'development'
+const url =
+  env === 'development'
+    ? process.env.DATABASE_DEV_URL
+    : process.env.DATABASE_URL
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url,
+    },
+  },
+})
 
 export const prismaClient: { prisma: PrismaClient } = {
   prisma,
